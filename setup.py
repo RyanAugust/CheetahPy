c6c15766119a58cc716811ed6d1c7c51401c6efe
RyanAugust <rad19206@yahoo.com>
2023-03-27 13:31:30 -0700

    lets make this a real thing

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="cheetahpy",
+    version="0.0.1",
+    author="Ryan Duecker",
+    author_email='ryan.duecker@yahoo.com',
+    description="Python wrapper for working with the Golden Cheetah API",
+    extras_require={
+        "test": [
+            "pytest"]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["PyYAML", "pandas","requests",],
+    url="https://github.com/RyanAugust/CheetahPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
