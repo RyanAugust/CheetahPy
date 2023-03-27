3305bb15756d5231e77e28cc9e190b1437593b5f
RyanAugust <rad19206@yahoo.com>
2023-03-27 13:36:45 -0700

    missed a few new lines

--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ r = gc.get_activities(athlete='Ryan Duecker'
                       ,end_date="2022/01/30"
                       ,metrics=['Duration','Distance','TSS','NP','IF']
                       ,metadata=['Sport','Workout_Code','Workout_Title','Shoes','Frame','Indoor'])
+print('successfully retrieved filtered athlete activities with custom metrics and metadata appended')
 
 r = gc.get_zones(athlete='Ryan Duecker', _for='hr', sport='Run')
-print(r)
+print('successfully retrieved athlete hr zones for running')
