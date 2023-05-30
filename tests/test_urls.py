2cc752fcf0f0dbf85a22f10cf3faf7e1f12f38a4
RyanAugust <rad19206@yahoo.com>
2023-05-30 12:33:31 -0700

    reformulate to naming

---
+++ tests/test_urls.py
@@ -0,0 +1,23 @@
+import pytest
+
+from cheetahpy import CheetahPy
+
+# r = CheetahPy.get_activities(athlete='Ryan Duecker'
+#                       ,start_date="2022/01/20"
+#                       ,end_date="2022/01/30"
+#                       ,metrics=['Duration','Distance','TSS','NP','IF']
+#                       ,metadata=['Sport','Workout_Code','Workout_Title','Shoes','Frame','Indoor'])
+# print('successfully retrieved filtered athlete activities with custom metrics and metadata appended')
+
+# r = CheetahPy.get_zones(athlete='Ryan Duecker', _for='hr', sport='Run')
+# print('successfully retrieved athlete hr zones for running')
+
+def test_base_url():
+    cp = CheetahPy()
+    result = cp.urls.base_url
+    assert result == "http://localhost:12021"
+
+if __name__ == '__main__':
+    test_base_url()
+
+    print("Everything passed")
