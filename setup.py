e3671d5bb4644d2c53ded5852aa4543541867686
RyanAugust <rad19206@yahoo.com>
2023-06-07 20:33:26 -0700

    move linting and testing dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
-import setuptools
+#!/usr/bin/env python3
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import os
+from setuptools import setup, find_packages
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
-setuptools.setup(
+setup(
     name="cheetahpy",
     version="0.4.0",
     author="Ryan Duecker",
     author_email='ryan.duecker@yahoo.com',
     description="Python wrapper for working with the Golden Cheetah API & opendata",
-    extras_require={
-        "test": [
-            "pytest"]
-    },
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["PyYAML", "pandas","requests",],
+    install_requires=['pandas','requests'],
+    python_requires='>=3.8',
     url="https://github.com/RyanAugust/CheetahPy",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
     ],
+    extras_require={
+        'linting': [
+            "flake8",
+            "pylint",
+            "mypy",
+            "pre-commit",
+        ],
+        "testing": [
+            "pytest"
+            ]
+    },
+    
 )
